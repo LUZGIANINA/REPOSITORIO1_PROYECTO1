e984bb3e6f4dde51ffc563e9961001ca0f1e08a7
LUZGIANINA <peqush@gmail.com>
2026-05-24 18:32:11 -0500

    Update welcome message for Session 1

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.sidebar.title("Busqueda")
 sesion = st.sidebar.selectbox("Seleccione una sesión", ["Sesión 1","Sesión 2","Sesión 3","Sesión 4"] )
 
 if sesion == "Sesión 1":
-  st.write("Bienvenido la sesión 1")
+  st.write("Bienvenidos al Explorador")
   st.image("Python_logo.png" )
 
 elif sesion == "Sesión 2":
