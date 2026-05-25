33c37d5e6d0275e8f0b8710624d37e46fcbe20df
LUZGIANINA <peqush@gmail.com>
2026-05-24 19:18:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,16 @@ st.sidebar.image("logo cv.png")
 
 st.sidebar.title("Busqueda")
 
-sesion = st.sidebar.selectbox("Seleccione una sesión", ["Inicio","Buscar Producto","Stock","Ingreso"] )
+sesion = st.sidebar.selectbox("Seleccione una sesión", ["Inicio","Sesión 2","Stock","Ingreso"] )
 
 if sesion == "Inicio":
   st.write("Bienvenidos al Explorador")
   st.image("Python_logo.png" )
 
-elif sesion == "Buscar producto":
-  st.write("Iniciamos la busqueda")
+elif sesion == "Sesión 2":
+  st.write("Bienvenido la sesión 2")
 
-  precio = st.number_input("Ingrese el código del producto", min_value = 0 , max_value = 5000 , value = 1200)
+  precio = st.number_input("Ingrese el precio del producto", min_value = 0 , max_value = 5000 , value = 1200)
   descuento = st.number_input("Ingrese el descuento del producto del 0 al 100% ", min_value = 0 , max_value = 100 )
 
   precio_final_producto = precio - (precio*(descuento/100))
