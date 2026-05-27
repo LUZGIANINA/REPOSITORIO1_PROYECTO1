dfc21de70baa9131bc5540a5cb11fd54339e9793
LUZGIANINA <peqush@gmail.com>
2026-05-26 23:22:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -31,12 +31,7 @@ elif opción == "Agregar producto":
 
     if st.button("Guardar Producto"):
 
-        Inventario.append([
-            codigo,
-            nombre,
-            stock,
-            precio
-        ])
+        Inventario.append([codigo,nombre,stock,precio])
 
         st.success("✅ Producto agregado correctamente")
 
